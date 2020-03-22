8d6111046952266be5493f7210ce2994b249a9af
Ewelina Mysiak <Ewelina.Mysiak@assecobs.pl>
2020-03-22 20:41:28 +0100

    Updates

---
+++ precache-manifest.ae424d927a31a95194767b8082599ce9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f036a0dc85d462e76c8babf92d6875b",
+    "url": "/firstReactApp/index.html"
+  },
+  {
+    "revision": "f695669c5920df42108f",
+    "url": "/firstReactApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "716c495071d66fad824a",
+    "url": "/firstReactApp/static/js/2.c6f00f49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/firstReactApp/static/js/2.c6f00f49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f695669c5920df42108f",
+    "url": "/firstReactApp/static/js/main.3ede6d22.chunk.js"
+  },
+  {
+    "revision": "0af6cd6c584d1935cc1c",
+    "url": "/firstReactApp/static/js/runtime-main.d060e407.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/firstReactApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3710922e38a9132370328f76f2b5ae29.js"
+  "/firstReactApp/precache-manifest.ae424d927a31a95194767b8082599ce9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/firstReactApp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
