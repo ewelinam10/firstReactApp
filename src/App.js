abd9e4017cbc703a5a3b7d6042c0a09b23019392
Ewelina Mysiak <Ewelina.Mysiak@assecobs.pl>
2020-03-22 23:30:29 +0100

    Stan komponentu - dodawanie nowych taksów w todolist

--- src/App.js
+++ src/App.js
@@ -2,17 +2,36 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 class MyList extends Component{
+  state = {
+    tasks: this.props.tasks,
+    draft : ''
+  }
+
+  updateDraft = (event) => {
+    this.setState({draft : event.target.value})
+  }
+
+  addTask = () => {
+    let newTasks = this.state.tasks;
+    newTasks.push(this.state.draft);
+    this.setState({tasks : newTasks, draft : ''})
+  }
+
   render(){
     return (
       <div>
         <h1> {this.props.title}</h1>
-        {this.props.tasks.map(task => 
+        {this.state.tasks.map(task => 
         <div><p> {task}</p></div>
         )}
+        <input type= 'text' onChange={this.updateDraft} />
+        <button onClick={this.addTask}>Add new task</button>
       </div>
     )
   }
 }
+
+
 class App extends Component {
  myTasks = [
    "Buy books",
