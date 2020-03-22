33e083ea3de50f00e05c48319a5acbb670d3d0d2
Ewelina Mysiak <Ewelina.Mysiak@assecobs.pl>
2020-03-22 20:39:50 +0100

    Updates

---
+++ precache-manifest.3710922e38a9132370328f76f2b5ae29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b13691694ef3024db109af112cf563e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a731ee2fe988e512fdac",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "716c495071d66fad824a",
+    "url": "/static/js/2.c6f00f49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c6f00f49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a731ee2fe988e512fdac",
+    "url": "/static/js/main.c249010d.chunk.js"
+  },
+  {
+    "revision": "c000c4c764c21e003eb3",
+    "url": "/static/js/runtime-main.677aa09c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
